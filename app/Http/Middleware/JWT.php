06b7b51ba9f89b2955ab791e17bc9c4c17e32ddd
preguajoshua <preguajoshua@gmail.com>
2022-03-03 18:52:53 +0800

    Added HHTPS if ENV is production

--- app/Http/Middleware/JWT.php
+++ app/Http/Middleware/JWT.php
@@ -16,6 +16,10 @@ class JWT
      */
     public function handle($request, Closure $next)
     {
+        if (!$request->secure() && App::environment() === 'production') {
+            return redirect()->secure($request->getRequestUri());
+        }
+        
         JWTAuth::parseToken()->authenticate();
         return $next($request);
     }
